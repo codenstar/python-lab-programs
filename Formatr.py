88e01de9fd2cb1b9e89aedfa553000528bba2367
Karthikeya CH <84000816+codenstar@users.noreply.github.com>
2021-05-21 15:28:25 +0530

    Add files via upload

---
+++ Formatr.py
@@ -0,0 +1,22 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> name="CSE-A"
+print("Hello {} !How are you?".format(name))
+age=19
+print("Age is %d" %(age))
+print("Hello %s !Your Age is %d"%(name,age))
+per=8.3
+print("Hello %s !Your Age is %d and your grade is %.2f"%(name,age,per))
+a=66
+print("hexa %x"%(a))
+print("hexa %X"%(a))
+print("char %c"%(a))
+print("oct %o"%(a))
+#outputHello CSE-A !How are you?
+Age is 19
+Hello CSE-A !Your Age is 19
+Hello CSE-A !Your Age is 19 and your grade is 8.90
+hexa 41
+hexa 41
+char A
+oct 101
